b944af639e85d78067909a633876bbec791505de
The-lady-developer <manvisinghwal123@gmail.com>
2018-09-01 13:04:58 +0530

    react-router-demo

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const About =() => {
+    return(
+        <div>
+            <p>Hey there! I am Manvi :)</p>
+        </div>
+    )
+}
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Contact =() => {
+    return(
+        <div>
+            <p>for more information contact at abc@gmail.com</p>
+        </div>
+    )
+}
+export default Contact;
